f0ba96c86469dbd1c46b8ca40cd2b79f54f4f28a
BhavitaP03 <145675065+BhavitaP03@users.noreply.github.com>
2024-03-04 16:31:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,28 @@
+from flask import Flask, request, jsonify
+from PIL import Image
+import numpy as np
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing.image import img_to_array, load_img
 
+app = Flask(__name__)
+model = load_model('your_model.h5')  # Replace with the path to your model
+
+class_labels = ['melanoma', 'nevus']
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    if request.method == 'POST':
+        file = request.files['image']
+        img = Image.open(file)
+        img = img.resize((150, 150))  # Adjust this to match the input size of your model
+        img = img_to_array(img)
+        img = np.expand_dims(img, axis=0)
+        img /= 255.0  # Normalize pixel values to the range [0, 1]
+
+        predictions = model.predict(img)
+        predicted_class = class_labels[int(round(predictions[0][0]))]
+
+        return jsonify({'predicted_class': predicted_class, 'class_probabilities': predictions.tolist()})
+
+if __name__ == '__main__':
+    app.run(debug=True)
